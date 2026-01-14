77ddae965d2ed70266b31eab382f7daf7a748680
Dominic Rodemer <dominic.rodemer@me.com>
2026-01-14 11:09:18 +0100

    Update qstack to 0.1.0

--- Formula/qstack.rb
+++ Formula/qstack.rb
@@ -2,7 +2,7 @@ class Qstack < Formula
   desc "Minimal, scriptable task and issue tracker for agent-driven workflows"
   homepage "https://github.com/domzilla/qstack"
   url "https://github.com/domzilla/qstack/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "ca25e2bd5aaf695492b748e49b2a11c0c7fa7c605dc42ef73719a582a490ab66"
   license "MIT"
   head "https://github.com/domzilla/qstack.git", branch: "master"
 
