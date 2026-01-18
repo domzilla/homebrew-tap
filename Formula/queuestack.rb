3b696d689996849d3d063cbe525d2a9b6e47355f
Dominic Rodemer <dominic.rodemer@me.com>
2026-01-18 10:40:25 +0100

    Rename project

--- Formula/queuestack.rb
+++ Formula/queuestack.rb
@@ -1,26 +1,23 @@
-class Qstack < Formula
+class Queuestack < Formula
   desc "Minimal, scriptable task and issue tracker for agent-driven workflows"
-  homepage "https://github.com/domzilla/qstack"
-  url "https://github.com/domzilla/qstack/archive/refs/tags/v0.3.0.tar.gz"
+  homepage "https://github.com/domzilla/queuestack"
+  url "https://github.com/domzilla/queuestack/archive/refs/tags/v0.3.0.tar.gz"
   sha256 "8cddbbc2659fd225d79dca859c4fbd5641726495967fd9fd472c094677319141"
   license "MIT"
-  head "https://github.com/domzilla/qstack.git", branch: "master"
+  head "https://github.com/domzilla/queuestack.git", branch: "master"
 
-  bottle do
-    root_url "https://github.com/domzilla/qstack/releases/download/v0.3.0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "90e1dee7f6e4e261d338c811755c61272bab6367d0989bb12f551e1fd195944f"
-    sha256 cellar: :any_skip_relocation, sonoma: "44c707f0567d2319a58e5015839e15c7a5e97a6c2a3f30b2b7453f5dbd9ddc06"
-  end
+  # Bottles will be added by CI on next release
+  # (v0.3.0 bottles were built with old binary name)
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
 
-    generate_completions_from_executable(bin/"qstack", "completions")
+    generate_completions_from_executable(bin/"qs", "completions")
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/qstack --version")
+    assert_match version.to_s, shell_output("#{bin}/qs --version")
   end
 end
